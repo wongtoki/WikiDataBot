2d5bffb68570393826b1984f33c9eacc539bf742
Toki Wong <wongtoki5@gmail.com>
2019-10-30 10:52:22 +0100

    update on the sparql.py

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 from agent import Agent
 import requests
 import json
+from wikidata.wikidata.sparql.sparql import Courior
 
 agent = Agent('SDHSJALSN','en')
 
-def console():
+def console(courior):
 	userInput = input("Talk to me:")
 	if userInput != "EXIT":
 		response = agent.ask_json(userInput)
 		parsed = json.loads(response.text)
-		print(json.dumps(parsed, indent=2, sort_keys=True))
-		console()
+		delivery = courior.deliver(parsed)
+		print(delivery)
+		console(courior)
 
 def main():
 
 	#Get a response from the dialogflow agent
 	# res = agent.ask_response_text("Hello")
 	# print(res)
-
-	console()
+	courior = Courior()
+	console(courior)
 
 	
 
--- wikidata/wikidata/sparql/sparql.py
+++ wikidata/wikidata/sparql/sparql.py
@@ -21,7 +21,13 @@ class Courior:
         value = res['results']
         return value
 
-    def deliver(self, intent, moviename="", date=datetime.today()):
+    def deliver(self, response, moviename="", date=datetime.today()):
+
+        
+
+
+        intent_name = response["intent"]["displayName"]
+        default_response = response["fulfillmentText"]
 
         # Creating the universal parameter object.
         parameters = Courior.Parameters()
@@ -30,18 +36,29 @@ class Courior:
 
         # Assign function names to viariables so they won't be called during assignment
         ask_oscar_winner_movie = self.__query_oscar_movies
-        ask_academy_award_winner = self.__query_academy_award_winner
+        ask_academy_award_winner_male = self.__query_academy_award_winner_male
+        ask_academy_award_winner_female = self.__query_academy_award_winner_female
         ask_release_date = self.__query_movie_release_date
+        ask_oscar_winner_director = self.__query_oscar_winner_director
 
         # Using a dictionary for easy intent response
         packages = {
             "ask_oscar_winner": ask_oscar_winner_movie,
-            "ask_academy_winner": ask_academy_award_winner,
-            "ask_release_date": ask_release_date
+            "ask_oscar_winner_male": ask_academy_award_winner_male,
+            "ask_oscar_winner_female": ask_academy_award_winner_female,
+            "ask_release_date": ask_release_date,
+            "ask_oscar_winner_director": ask_oscar_winner_director
         }
 
         # Calling the function assigned to the dictionary key
-        return packages[intent](parameters)
+        try:
+            res = packages[intent_name](parameters)
+            if res == None:
+                return default_response
+        except:
+            return default_response
+
+        return res
 
     def __query_oscar_movies(self, params):
         year = str(params.date.year)
@@ -60,7 +77,7 @@ class Courior:
         res = self.__send_query(query)
         return res['bindings'][0]['movieLabel']['value']
 
-    def __query_academy_award_winner(self, params):
+    def __query_academy_award_winner_male(self, params):
         year = str(params.date.year)
         query = """
             SELECT ?actor ?actorLabel ?date ?forWork ?forWorkLabel
@@ -75,7 +92,26 @@ class Courior:
             SERVICE wikibase:label { bd:serviceParam wikibase:language "en,fr" . }
             }
         """ % (year, year)
-        res = self.__sends_query(query)
+        res = self.__send_query(query)
+        return res['bindings'][0]['actorLabel']['value']
+
+    def __query_academy_award_winner_female(self, params):
+        year = str(params.date.year)
+        query = """
+            SELECT ?actor ?actorLabel ?date ?forWork ?forWorkLabel
+            WHERE
+            {
+            # find a human
+            ?actor wdt:P31 wd:Q5 .
+            ?actor p:P166 ?awardstatement .
+            ?awardstatement ps:P166 wd:Q103618 .
+            ?awardstatement pq:P585 ?date .
+            ?awardstatement pq:P1686 ?forWork .
+            FILTER((?date >= "%s-01-01T00:00:00Z"^^xsd:dateTime) && (?date <= "%s-12-31T00:00:00Z"^^xsd:dateTime))
+            SERVICE wikibase:label { bd:serviceParam wikibase:language "en,fr" . }
+            }
+        """ % (year, year)
+        res = self.__send_query(query)
         return res['bindings'][0]['actorLabel']['value']
 
     def __query_movie_release_date(self, params):
@@ -92,19 +128,45 @@ class Courior:
         """ % (params.moviename.lower())
 
         res = self.__send_query(query)
-
+        print(res)
         results = []
         for v in res["bindings"]:
             output = {
-                "moviename":v["itemLabel"]["value"],
-                "year":v["year"]["value"]
+                "moviename": v["itemLabel"]["value"],
+                "year": v["year"]["value"]
             }
             results.append(output)
 
         return results
 
+    def __query_oscar_winner_director(self, params):
+        year = str(params.date.year)
+        query = """
+            SELECT ?actor ?actorLabel ?date ?forWork ?forWorkLabel
+            WHERE
+            {
+            # find a human
+            ?actor wdt:P31 wd:Q5 .
+            # Now comes the statements/qualifiers magic:
+            ?actor p:P166 ?awardstatement .
+            ?awardstatement ps:P166 wd:Q103360 .
+            ?awardstatement pq:P585 ?date .
+            ?awardstatement pq:P1686 ?forWork .
+            FILTER((?date >= "{%s}-01-01T00:00:00Z"^^xsd:dateTime) && (?date <= "%s-12-31T00:00:00Z"^^xsd:dateTime))
+            SERVICE wikibase:label { bd:serviceParam wikibase:language "en,fr" . }
+            }
+        """ % (year, year)
+        res = self.__send_query(query)
+        res = res["bindings"]
+        return res
+
+    def __query_genre(self, params):
+        pass
+
 
 if __name__ == "__main__":
     courior = Courior()
-    delivery = courior.deliver("ask_release_date", moviename="Avatar")
+
+
+    delivery = courior.deliver()
     print(delivery)
