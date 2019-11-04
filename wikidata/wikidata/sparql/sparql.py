75e8063ee16599ed7d2280e04a7eeb17f6deb6d4
louisdebruijn <l.e.d.de.bruijn@student.rug.nl>
2019-11-04 15:14:50 +0100

    print statements

--- wikidata/wikidata/sparql/sparql.py
+++ wikidata/wikidata/sparql/sparql.py
@@ -89,12 +89,13 @@ class Courier:
 
     def __get_movie_name(self, response):
         movie_string = response["parameters"]["fields"]["movie_name"]["stringValue"]
+        print('movie_string from DialogFlow:', movie_string)
         '''removing all exclamation marks, question marks and punctuation.'''
-        patterns = ['["\'^!.?]+']
+        patterns = ['[^!.?]+']
         for p in patterns:
             movie_string = re.findall(p, movie_string)[0]
         # movie_string = movie_string.replace("?", "") # removing all question marks
-        print('movie_name from DialogFlow:', movie_string)
+        print('movie_string after processed movie_name:', movie_string)
         return movie_string
 
 
